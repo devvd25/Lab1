874f5de199f7ee0aeb9c97e997f6d0835acdf799
devv25 <devv25@gmail.com>
2026-01-08 16:59:39 +0700

    Bài 5: Trả về danh sách (List) ; http://localhost:8080/api/students

--- studentmanager/src/main/java/com/example/studentmanager/StudentmanagerApplication.java
+++ studentmanager/src/main/java/com/example/studentmanager/StudentmanagerApplication.java
@@ -1,5 +1,8 @@
 package com.example.studentmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,31 +19,41 @@ public class StudentmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagerApplication.class, args);
 	}
-	
+	//Bài 1: Tạo Spring Boot REST API đơn giản (Hello API)
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello Spring Boot API";
 	}
+	//Bài 2: API nhận tham số (Request Param)
 	@GetMapping("/greet")
 	public String greet(@RequestParam String name) {
     	return "Xin chào " + name;
 
 	}
+
 	@GetMapping("/students/search")
 	public String search(
         @RequestParam String keyword,
         @RequestParam(defaultValue = "1") int page) {
     return "keyword=" + keyword + ", page=" + page;
 }
-	
+	//Bài 3: API với Path Variable
 	@GetMapping("/student/{id}")
 	public String getStudent(@PathVariable int id) {
 		return "Sinh viên có mã: " + id;
 	}
-	
+	// Bài 4: Trả về JSON Object
 	@GetMapping("/student")
 	public Student getStudent() {
 		return new Student(1, "Nguyễn Văn A", 20);
 	}
+	//Bài 5: Trả về danh sách (List)
+    @GetMapping("/students")
+	public List<Student> getStudents() {
+    List<Student> list = new ArrayList<>();
+    list.add(new Student(1, "A", 20));
+    list.add(new Student(2, "B", 21));
+    	return list;
 
+}
 }
