561a4196bd84517508afdfc1e3b553fec6627b20
devv25 <devv25@gmail.com>
2026-01-08 16:52:38 +0700

    Bài 3: API với Path Variable👉 Truy cập url gọi api:  /students/10

--- studentmanager/src/main/java/com/example/studentmanager/StudentmanagerApplication.java
+++ studentmanager/src/main/java/com/example/studentmanager/StudentmanagerApplication.java
@@ -3,6 +3,7 @@ package com.example.studentmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +32,10 @@ public class StudentmanagerApplication {
         @RequestParam(defaultValue = "1") int page) {
     return "keyword=" + keyword + ", page=" + page;
 }
+	
+	@GetMapping("/student/{id}")
+	public String getStudent(@PathVariable int id) {
+		return "Sinh viên có mã: " + id;
+	}
+
 }
